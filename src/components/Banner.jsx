0aa79f184ee5bc8c5e914463428e93dfce4be37a
Ajinkya <patilajink2005@gmail.com>
2025-05-20 18:46:57 +0530

    banner changed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='h-[50vh] bg-cover bg-center bg-no-repeat flex items-end' style={{backgroundImage: `url(https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/6720/1366720-i-c30300a11e8d)`}}>
+    <div className='h-[50vh] bg-black bg-center bg-no-repeat flex items-end' style={{backgroundImage: `url(https://m.media-amazon.com/images/I/81C5A5waXfL._AC_UF1000,1000_QL80_.jpg)`}}>
       <div className='text-white w-full text-center bg-black p-1'>
-        Avengers End Game
+        
       </div>
     </div>
   )
